66d112e465ca9e9f53744f73a2bc45acd98c9ba2
James Hunt <james@niftylogic.com>
2014-12-13 23:43:59 -0500

    Bring mmap.c in line with article

--- mmap.c
+++ mmap.c
@@ -3,24 +3,24 @@
 int main(int argc, char **argv)
 {
 	/* inert map (never modified) */
-	char *inert = mmap(NULL, 32 KB,
+	char *inert = mmap(NULL, 16 KB,
 		PROT_READ|PROT_WRITE,
 		MAP_ANONYMOUS|MAP_PRIVATE,
 		-1, 0);
 
 	/* anonymous, private mmap */
-	char *anon_priv = mmap(NULL, 64 KB,
+	char *anon_priv = mmap(NULL, 32 KB,
 		PROT_READ|PROT_WRITE,
 		MAP_ANONYMOUS|MAP_PRIVATE,
 		-1, 0);
-	randomize(anon_priv, 64 KB);
+	randomize(anon_priv, 32 KB);
 
 	/* anonymous, shared map */
-	char *anon_shared = mmap(NULL, 128 KB,
+	char *anon_shared = mmap(NULL, 64 KB,
 		PROT_READ|PROT_WRITE,
 		MAP_ANONYMOUS|MAP_SHARED,
 		-1, 0);
-	randomize(anon_shared, 128 KB);
+	randomize(anon_shared, 64 KB);
 
 	/* private, file-backed map */
 	int fd = open("data/256k", O_RDWR);
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
 		PROT_READ|PROT_WRITE,
 		MAP_PRIVATE,
 		fd, 0);
-	randomize(file, 256 KB);
+	randomize(file, 128 KB);
 
 	return interlude();
 }
